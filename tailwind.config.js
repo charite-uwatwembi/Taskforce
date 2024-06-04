266bb735fa21231015616bb9141c295d2a471d71
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-06-04 22:52:42 +0200

    TaskForce

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  variants: {},
+  plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.grayscale-on-hover': {
+          '&:hover': {
+            filter: 'grayscale(100%)',
+          },
+        },
+      };
+      addUtilities(newUtilities, ['responsive']);
+    },
+  ],
+};
+
 
