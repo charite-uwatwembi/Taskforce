39b9c0ade763ba7aebddca838c6582cca215b653
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-06-05 22:59:40 +0200

    Updating input.css and challenges folder

--- Challenges/stringTransform.js
+++ Challenges/stringTransform.js
@@ -4,20 +4,25 @@
 // ● If the length of the string is divisible by both 3 and 5 (i.e., divisible by 15), perform
 // both operations in the order specified above.
 
-const stringManipulation = (str) => {
-    // Check if the length of the string is divisible by 3, 5, or both
-    if (str.length % 3 === 0) {
-        return str.split("").reverse().join("");
-    } else if (str.length % 5 === 0) {
-        return str.split("").map((char) => char.charCodeAt(0)).join(" ");
-    } else if (str.length % 15 === 0) {
-        return str.split("").reverse().map((char) => char.charCodeAt(0)).join("");
-    } else {
-        // If the length of the string is not divisible by 3, 5, or both, return the original string
-        return str;
+const stringManipulation = str => {
+    const length = str.length;
+
+    if (length % 15 === 0) {
+        return `"${[...str].reverse().join('')}"`;
     }
+
+    if (length % 3 === 0) {
+        return `"${[...str].reverse().join('')}"`;
+    }
+
+    if (length % 5 === 0) {
+        return `"${[...str].map(char => char.charCodeAt(0)).join(' ')}"`;
+    }
+
+    return `"${str}"`;
 };
 
-console.log(stringManipulation("Hamburger"));
-console.log(stringManipulation("Pizza"));
-console.log(stringManipulation("Chocolate Chip Cookie"));
+// Test cases
+console.log(stringManipulation("Hamburger")); 
+console.log(stringManipulation("Pizza")); 
+console.log(stringManipulation("Chocolate Chip Cookie")); 
